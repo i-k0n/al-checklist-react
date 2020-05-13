00eff9bc902afadd039ad077c4c2545fef80a622
i-k0n <19754246+i-k0n@users.noreply.github.com>
2020-05-12 19:05:47 -0500

    added sorting filter component

---
+++ src/components/Sort.js
@@ -0,0 +1,42 @@
+import React from "react";
+import SortButton from "./SortButton";
+
+const onClick = () => {
+  console.log("You clicked a button");
+};
+
+const sortBy = [
+  "Vanguard",
+  "Main",
+  "DD",
+  "CL",
+  "CA",
+  "BB",
+  "CV",
+  "AR",
+  "SS",
+  "BM",
+];
+
+export const Sort = () => {
+  return (
+    <div id="sort">
+      <p className="sort-text">Sort By: </p>
+      {sortBy.map((sort) => {
+        return (
+          <SortButton onClick={onClick} sort={sort.toLowerCase()} key={sort}>
+            {sort}
+          </SortButton>
+        );
+      })}
+      <button id="collapse-filters">
+        <span>Collapse&nbsp;Filters</span>
+        <img src="./assets/img/collapse-arrow.svg" alt="collaps arrow" />
+      </button>
+      <input id="hide-toggle" type="checkbox" />
+      <label id="hide-owned" data-filter="completed" htmlFor="hide-toggle">
+        Hide Owned
+      </label>
+    </div>
+  );
+};

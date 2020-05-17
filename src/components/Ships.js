31937e08d8bb69fea6cdb66256bc2450c5f53fc2
i-k0n <19754246+i-k0n@users.noreply.github.com>
2020-05-16 21:35:32 -0500

    removed click handler

--- src/components/Ships.js
+++ src/components/Ships.js
@@ -2,20 +2,10 @@ import React, { useState, useEffect } from "react";
 import Card from "./Card";
 
 export const Ships = (props) => {
-  // const { ships, setShips } = useState({ data });
-  // console.log("data: ", props.data);
-  // const renderShips = useEffect(() => {
-  //   console.log("ships: ", ships);
-  // }, [ships]);
-
-  const onClick = (ship, index) => {
-    console.log(`${ship.name}: ${index}`);
-  };
-
   return (
     <div className="ships-container">
       {props.data.map((ship, index) => {
-        return <Card ship={ship} index={index} key={index} onClick={onClick} />;
+        return <Card ship={ship} index={index} key={index} />;
       })}
     </div>
   );

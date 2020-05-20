93e0ffa4609ff1f988e13c08048317e4ce02393b
i-k0n <19754246+i-k0n@users.noreply.github.com>
2020-05-19 22:21:47 -0500

    store state in local storage to remember selected ships

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 
 export default function Card({ ship, index }) {
   const [checked, setChecked] = useState(ship.checked);
+  const [obtainedShips, setObtainedShips] = useState([]);
 
   let filters = [];
   if (
@@ -33,45 +34,87 @@ export default function Card({ ship, index }) {
   filters.push(ship.collection);
   filters.push(parseInt(ship.id) >= 3005 ? "retrofit" : "");
 
-  const onChange = () => {
-    setChecked(!checked);
+  const onChange = (e) => {
+    setChecked(e.target.checked);
+  };
+
+  const loadLocalStorage = () => {
+    // get obtainedShips array from localStorage and load as data
+    // Set() removes any duplicate entries so our array always has unique values
+    let data = Array.from(
+      new Set(JSON.parse(localStorage.getItem("obtainedShips")))
+    );
+    // populate obtainedShips array with localStorage
+    setObtainedShips([...data]);
+    console.log("localStorage data: ", data);
+    // loop through localStorage data
+    for (let i = 0; i < data.length; i++) {
+      // add completed class to ships in array
+      document
+        .querySelector(`.ship[data-id='${data[i]}']`)
+        .classList.add("completed");
+      // get matching inputs and mark as checked
+      document
+        .querySelector('label[data-id="009"] > input')
+        .setAttribute("checked", true);
+    }
+    // console.log('saved ships: ', obtainedShips);
+    console.log("localStorage loaded!");
   };
 
   useEffect(() => {
-    console.log(`${ship.name}: ${index} isChecked: ${checked}`);
+    loadLocalStorage();
+  }, []);
 
+  useEffect(() => {
+    console.log(`${ship.name} checked: ${checked}`);
+  }, [checked]);
+
+  useEffect(() => {
+    // click ship
+    // set clicked ship's class as "completed"
+    // get clicked ship's id
+    // pass that id to an array of ship ids
+    // update state with array of ids
+    // push that state to local storage
+
+    const shipArr = [...obtainedShips];
+    let i = shipArr.indexOf(ship.id);
     if (checked) {
+      // add completed class to .ship label
       document
         .querySelector(`.ship[data-id='${ship.id}']`)
         .classList.add("completed");
-      // $(`.counter.${faction}`).addClass("flash-green");
-      // setTimeout(function() {
-      //     $(`.counter.${faction}`).removeClass("flash-green");
-      // }, 1000);
-      console.log(ship.faction);
+
+      // console.log(ship.faction);
       // add clicked id to obtainedShips array
-      // obtainedShips.push(id);
-      // store array in localStorage
-      // localStorage.setItem("obtainedShips", JSON.stringify(obtainedShips));
-      // console.log("ships array: ", obtainedShips);
+      if (i === -1) {
+        shipArr.push(ship.id);
+        setObtainedShips(shipArr);
+        console.log("add: ", ship.name, ship.id);
+        console.log("ship array: ", shipArr);
+
+        // store array in localStorage
+        localStorage.setItem("obtainedShips", JSON.stringify(shipArr));
+      }
     } else {
+      // remove completed class from .ship label
       document
         .querySelector(`.ship[data-id='${ship.id}']`)
         .classList.remove("completed");
-      // $(`.counter.${faction}`).addClass("flash-red");
-      // setTimeout(function() {
-      //     $(`.counter.${faction}`).removeClass("flash-red");
-      // }, 1000);
-      //   console.log(name + " lost!")
+
+      console.log(ship.name + " lost!");
       // search obtainedShips array for the clicked id
-      // let i = obtainedShips.indexOf(id);
-      // console.log("ships array: ", obtainedShips);
+
+      console.log("ships array: ", shipArr);
       // console.log("localstorage: ", localStorage.obtainedShips);
-      // if (i !== -1) {
-      //     obtainedShips.splice(i, 1);
-      //     localStorage.setItem("obtainedShips", JSON.stringify(obtainedShips));
-      //     console.log("removed");
-      // }
+      if (i !== -1) {
+        shipArr.splice(i, 1);
+        setObtainedShips(shipArr);
+        localStorage.setItem("obtainedShips", JSON.stringify(shipArr));
+        console.log("removed: ", ship.name, ship.id);
+        console.log("ship array: ", shipArr);
+      }
     }
   }, [checked]);
 
@@ -92,7 +135,7 @@ export default function Card({ ship, index }) {
         onChange={(e) => {
           // prevent default event from running render twice
           // e.preventDefault();
-          onChange();
+          onChange(e);
         }}
       />
       <span

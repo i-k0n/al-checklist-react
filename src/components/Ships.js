b9fb851eb734e18815bf4e7264447f428fcaf0b6
i-k0n <19754246+i-k0n@users.noreply.github.com>
2020-05-22 18:04:40 -0500

    store user's choice to hide obtained ships in localStorage

--- src/components/Ships.js
+++ src/components/Ships.js
@@ -1,15 +1,9 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect } from "react";
 import Card from "./Card";
 
 export const Ships = (props) => {
   const [obtainedShips, setObtainedShips] = useState([]);
   const shipArr = obtainedShips;
-  // const wrapperSetObtainedShips = useCallback(
-  //   (val) => {
-  //     setObtainedShips(val);
-  //   },
-  //   [setObtainedShips]
-  // );
 
   const loadLocalStorage = () => {
     // get obtainedShips array from localStorage and load as data
@@ -19,7 +13,6 @@ export const Ships = (props) => {
       new Set(JSON.parse(localStorage.getItem("obtainedShips")))
     );
     // populate obtainedShips array with localStorage
-    // setObtainedShips([...data]);
     console.log("localStorage data: ", data);
     // loop through localStorage data
     for (let i = 0; i < data.length; i++) {
@@ -35,6 +28,22 @@ export const Ships = (props) => {
     // console.log('saved ships: ', obtainedShips);
     console.log("localStorage loaded!");
     setObtainedShips([...data]);
+    hideIfHidden();
+  };
+
+  const hideIfHidden = () => {
+    console.log("hideIfHidden ran");
+    let hideShips = localStorage.getItem("isHidden");
+    if (hideShips === "true") {
+      document.querySelector("#hide-toggle").checked = true;
+      console.log(
+        "hideToggle checked? ",
+        document.querySelector("#hide-toggle").checked
+      );
+      document
+        .querySelectorAll(".completed")
+        .forEach((ship) => ship.classList.remove("show"));
+    }
   };
   useEffect(() => {
     loadLocalStorage();
@@ -43,15 +52,7 @@ export const Ships = (props) => {
   return (
     <div className="ships-container">
       {props.data.map((ship, index) => {
-        return (
-          <Card
-            ship={ship}
-            index={index}
-            key={index}
-            shipArr={shipArr}
-            // obtainedShipsSetter={wrapperSetObtainedShips}
-          />
-        );
+        return <Card ship={ship} index={index} key={index} shipArr={shipArr} />;
       })}
     </div>
   );

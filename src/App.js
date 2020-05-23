a6e27e4e7f83de336870cb6948b9126103965a7f
i-k0n <19754246+i-k0n@users.noreply.github.com>
2020-05-23 14:15:54 -0500

    added to-do list of features to fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import { Filters } from "./components/Filters";
 import { Ships } from "./components/Ships";
 import { Sort } from "./components/Sort";
@@ -6,13 +6,43 @@ import { Counter } from "./components/Counter";
 import * as shipsData from "./data/ships4.json";
 
 function App() {
+  const [faction, setFaction] = useState("");
   const data = shipsData.default;
+
+  const updateCounters = (faction) => {
+    console.log("faction: ", faction);
+    let factionCounter, factionCompleted;
+    if (faction !== "total") {
+      factionCounter = document.querySelectorAll(
+        '.ship[data-filters*="' + faction + '"]'
+      );
+      factionCompleted = document.querySelectorAll(
+        `.ship[data-filters*="${faction}"].completed`
+      );
+    } else {
+      factionCounter = document.querySelectorAll(
+        '.ship[data-filters]:not([data-filters*="collab"])'
+      );
+      factionCompleted = document.querySelectorAll(
+        '.ship.completed:not([data-filters*="collab"])'
+      );
+    }
+
+    document.querySelector(
+      "li.counter." + faction + " > span"
+    ).textContent = ` ${factionCompleted.length} / ${factionCounter.length}`;
+  };
+
   return (
     <Fragment>
       <div className="checklist-header">
         <Filters data={data} />
         <Sort data={data} />
-        <Counter data={data} />
+        <Counter
+          data={data}
+          updateCounters={updateCounters}
+          faction={{ faction, setFaction }}
+        />
       </div>
       <Ships data={data} />
     </Fragment>
@@ -20,3 +50,12 @@ function App() {
 }
 
 export default App;
+
+// to do
+// fix filter logic so the don't cancel out each other
+////   clear filters clears hide owned toggle
+////   search filter cancels hide owned toggle
+//   toggling off hideOwned cancels out filters and shows all obtained ships
+// allow for selecting of multiple filters to be additive
+// fix counters
+// enable sorting

97a9e4de987f2277f74eb50a212595e47ce4a900
i-k0n <19754246+i-k0n@users.noreply.github.com>
2020-05-24 16:16:50 -0500

    removed counter update function

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,85 +1,13 @@
-import React, { useEffect } from "react";
+import React from "react";
 
-const counters = [
-  {
-    full: "Eagle Union",
-    short: "eagle",
-  },
-  {
-    full: "Royal Navy",
-    short: "royal",
-  },
-  {
-    full: "Sakura Empire",
-    short: "sakura",
-  },
-  {
-    full: "Ironblood",
-    short: "ironblood",
-  },
-  {
-    full: "Dragon Empery",
-    short: "dragon",
-  },
-  {
-    full: "Sardegna Empire",
-    short: "sardegna",
-  },
-  {
-    full: "Northern Parliament",
-    short: "northern",
-  },
-  {
-    full: "Vichya Dominion",
-    short: "vichya",
-  },
-  {
-    full: "Iris Libre",
-    short: "iris",
-  },
-  {
-    full: "Other",
-    short: "other",
-  },
-  {
-    full: "Total",
-    short: "total",
-  },
-];
-
-export const Counter = () => {
-  const UpdateCounters = (faction) => {
-    useEffect(() => {
-      let factionCounter, factionCompleted;
-      if (faction !== "total") {
-        factionCounter = document.querySelectorAll(
-          '.ship[data-filters*="' + faction + '"]'
-        );
-        factionCompleted = document.querySelectorAll(
-          `.ship[data-filters*="${faction}"].completed`
-        );
-      } else {
-        factionCounter = document.querySelectorAll(
-          '.ship[data-filters]:not([data-filters*="collab"])'
-        );
-        factionCompleted = document.querySelectorAll(
-          '.ship.completed:not([data-filters*="collab"])'
-        );
-      }
-
-      document.querySelector(
-        "li.counter." + faction + " > span"
-      ).textContent = ` ${factionCompleted.length} / ${factionCounter.length}`;
-    }, [faction]);
-  };
-
+export const Counter = ({ counters }) => {
   return (
     <ul className="counters">
       {counters.map((counter) => {
         return (
           <li className={`counter ${counter.short}`} key={counter.full}>
             {counter.full}
-            <span>{UpdateCounters(counter.short)}</span>
+            <span></span>
           </li>
         );
       })}

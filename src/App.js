a829caeb1d254e8bba5dd1aeaa0fa4fbd1b959af
i-k0n <19754246+i-k0n@users.noreply.github.com>
2020-12-11 11:22:31 -0600

    feat: added state for hiding collab ships and obtained ships

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ function App() {
   const [sortType, setSortType] = useState("id");
   const [textFilter, setTextFilter] = useState("");
   const [currentFilter, setCurrentFilter] = useState("");
+  const [hideCollab, setHideCollab] = useState(false);
+  const [hideOwned, setHideOwned] = useState(JSON.parse(localStorage.getItem("isHidden")) || false);
+  const [collapseFilters, setCollapseFilters] = useState(false);
 
   return (
     <Fragment>
@@ -21,14 +24,35 @@ function App() {
           setCurrentFilter={setCurrentFilter}
           setTextFilter={setTextFilter}
           textFilter={textFilter}
+          collapseFilters={collapseFilters}
         />
+        <div className="collapse-filters-container">
+            <button id="collapse-filters" onClick={() => setCollapseFilters(prevState => !prevState)}>
+                <span>{!collapseFilters ? `Expand Filters` : `Collapse Filters`}</span>
+                <svg aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" className="svg-inline--fa fa-angle-double-up fa-w-10 fa-2x">
+                    <path 
+                      fill="currentColor" 
+                      d="M177 255.7l136 136c9.4 9.4 9.4 24.6 0 33.9l-22.6 22.6c-9.4 9.4-24.6 9.4-33.9 0L160 351.9l-96.4 96.4c-9.4 9.4-24.6 9.4-33.9 0L7 425.7c-9.4-9.4-9.4-24.6 0-33.9l136-136c9.4-9.5 24.6-9.5 34-.1zm-34-192L7 199.7c-9.4 9.4-9.4 24.6 0 33.9l22.6 22.6c9.4 9.4 24.6 9.4 33.9 0l96.4-96.4 96.4 96.4c9.4 9.4 24.6 9.4 33.9 0l22.6-22.6c9.4-9.4 9.4-24.6 0-33.9l-136-136c-9.2-9.4-24.4-9.4-33.8 0z" 
+                      className="">
+                    </path>
+                </svg>
+            </button>
+        </div>
         <Sort
           data={data}
           setSortType={setSortType}
           currentFilter={currentFilter}
           setCurrentFilter={setCurrentFilter}
+          hideCollab={hideCollab}
+          setHideCollab={setHideCollab}
+          hideOwned={hideOwned}
+          setHideOwned={setHideOwned}
         />
-        <Counter />
+        <Counter 
+          data={data}
+          faction={faction}
+          hideCollab={hideCollab}
+        />
       </div>
       <Ships
         data={data}
@@ -38,6 +62,9 @@ function App() {
         sortType={sortType}
         setSortType={setSortType}
         textFilter={textFilter}
+        hideCollab={hideCollab}
+        hideOwned={hideOwned}
+        setHideOwned={setHideOwned}
       />
     </Fragment>
   );

d87504bee5661db6e598e639ca88ee2993f4b682
i-k0n <19754246+i-k0n@users.noreply.github.com>
2020-12-16 13:47:44 -0600

    moved to separate file to clean up Filters.js

---
+++ src/components/Filters/filterData.js
@@ -0,0 +1,69 @@
+const factionFilters = [
+  {
+    full: "Eagle Union",
+    short: "eagle",
+  },
+  {
+    full: "Royal Navy",
+    short: "royal",
+  },
+  {
+    full: "Sakura Empire",
+    short: "sakura",
+  },
+  {
+    full: "Ironblood",
+    short: "ironblood",
+  },
+  {
+    full: "Dragon Empery",
+    short: "dragon",
+  },
+  {
+    full: "Sardegna Empire",
+    short: "sardegna",
+  },
+  {
+    full: "Northern Parliament",
+    short: "northern",
+  },
+  {
+    full: "Vichya Dominion",
+    short: "vichya",
+  },
+  {
+    full: "Iris Libre",
+    short: "iris",
+  },
+  {
+    full: "Universal Exchange",
+    short: "universal",
+  },
+];
+
+const typeFilters = [
+  "Vanguard",
+  "Main",
+  "DD",
+  "CL",
+  "CA",
+  "BB",
+  "CV",
+  "AR",
+  "SS",
+  "BM",
+];
+
+const rarityFilters = [
+  "Common",
+  "Rare",
+  "Elite",
+  "Super Rare",
+  "Ultra Rare",
+  "Collab",
+  "Retrofit",
+  "Drops",
+  "Drop Only",
+];
+
+export { factionFilters, typeFilters, rarityFilters };

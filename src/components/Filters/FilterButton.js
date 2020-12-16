6ac65d03e8c3c2a1f79d42a8627f7f5fcc048077
i-k0n <19754246+i-k0n@users.noreply.github.com>
2020-12-16 14:02:14 -0600

    fix: moved components to their own folders

--- src/components/Filters/FilterButton.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const FilterButton = ({ children, dataFilter, onClick }) => {
-  return (
-    <button className="button" onClick={onClick} data-filter={dataFilter}>
-      {children}
-    </button>
-  );
-};

b366b45cf0c6d76bd6b527bbefe8930515dd1cf5
i-k0n <19754246+i-k0n@users.noreply.github.com>
2020-12-16 14:04:48 -0600

    feat: added state for tracking filters, and an event handler for hiding filter menu when scrolling

--- src/App.js
+++ src/App.js
@@ -1,43 +1,107 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect, useRef } from "react";
 import Filters from "./components/Filters";
 import Ships from "./components/Ships";
 import Sort from "./components/Sort/Sort";
 import Counter from "./components/Counter";
 import * as shipsData from "./data/ships4.json";
+import { ChecklistHeader } from "./App.styles";
+import CollapseFilters from "./components/Filters/CollapseFilters"
+
+let collapseDelay = false;
 
 function App() {
   const [data, setData] = useState(shipsData.default);
   const [faction, setFaction] = useState("total");
   const [sortType, setSortType] = useState("id");
+  const [factionFilter, setFactionFilter] = useState([]);
+  const [typeFilter, setTypeFilter] = useState([]);
+  const [rarityFilter, setRarityFilter] = useState([]);
   const [textFilter, setTextFilter] = useState("");
   const [currentFilter, setCurrentFilter] = useState("");
   const [hideCollab, setHideCollab] = useState(false);
   const [hideOwned, setHideOwned] = useState(JSON.parse(localStorage.getItem("isHidden")) || false);
   const [collapseFilters, setCollapseFilters] = useState(false);
+  const [returnedShips, setReturnedShips] = useState(0);
+  // store filters container height in state since we have to let it render before we can grab the element height
+  const [filtersContainerHeight, setFiltersContainerHeight] = useState(0);
+
+  // use a ref to get the height of the DOM element
+  const filtersContainerRef = useRef(null);
+
+  const headerRef = useRef(null);
+  const counterRef = useRef(null);
+  
+  const handleCollapse = () => {
+    collapseDelay = true;
+    setCollapseFilters(prevState => !prevState);
+    setTimeout(() => collapseDelay = false, 1000);
+  }
+
+  // handle hiding of controls while scrolling
+  var prevScrollpos = window.pageYOffset;
+
+  const handleScroll =  () => {
+    var currentScrollPos = window.pageYOffset;
+
+    const controls = headerRef.current;
+    const counters = counterRef.current.offsetHeight;
+    var headerSize = controls.offsetHeight;
+    if (prevScrollpos > currentScrollPos) {
+      // if (scrolling up)
+      controls.style.top = "0";
+    } else {
+      // else (scrolling down)
+      // && if scrolling isn't caused by expanding/collapsing filters
+      if (prevScrollpos > headerSize && !collapseDelay) {
+        controls.style.top = "-" + (headerSize - counters - 5) + "px";
+      }
+    }
+    prevScrollpos = currentScrollPos;
+  };
+
+  // update state with element height once it has rendered to the DOM
+  useEffect(() => {
+    setFiltersContainerHeight(filtersContainerRef.current.clientHeight);
+  }, [filtersContainerHeight])
+
+  // create event listener to hide sort controls when scrolling the page
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    }
+    // eslint-disable-next-line
+  }, [])
+
+  useEffect(() => {
+    setReturnedShips(document.querySelectorAll('.ship').length);
+ })
 
   return (
     <Fragment>
-      <div className="checklist-header">
+      <ChecklistHeader className="checklist-header" ref={headerRef}>
         <Filters
           data={data}
           currentFilter={currentFilter}
           setCurrentFilter={setCurrentFilter}
+          setFactionFilter={setFactionFilter}
+          factionFilter={factionFilter}
+          setTypeFilter={setTypeFilter}
+          typeFilter={typeFilter}
+          setRarityFilter={setRarityFilter}
+          rarityFilter={rarityFilter}
           setTextFilter={setTextFilter}
           textFilter={textFilter}
           collapseFilters={collapseFilters}
+          filtersContainerHeight={filtersContainerHeight}
+          forwardedRef={filtersContainerRef}
+          returnedShips={returnedShips}
+          setReturnedShips={setReturnedShips}
         />
-        <div className="collapse-filters-container">
-            <button id="collapse-filters" onClick={() => setCollapseFilters(prevState => !prevState)}>
-                <span>{!collapseFilters ? `Expand Filters` : `Collapse Filters`}</span>
-                <svg aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" className="svg-inline--fa fa-angle-double-up fa-w-10 fa-2x">
-                    <path 
-                      fill="currentColor" 
-                      d="M177 255.7l136 136c9.4 9.4 9.4 24.6 0 33.9l-22.6 22.6c-9.4 9.4-24.6 9.4-33.9 0L160 351.9l-96.4 96.4c-9.4 9.4-24.6 9.4-33.9 0L7 425.7c-9.4-9.4-9.4-24.6 0-33.9l136-136c9.4-9.5 24.6-9.5 34-.1zm-34-192L7 199.7c-9.4 9.4-9.4 24.6 0 33.9l22.6 22.6c9.4 9.4 24.6 9.4 33.9 0l96.4-96.4 96.4 96.4c9.4 9.4 24.6 9.4 33.9 0l22.6-22.6c9.4-9.4 9.4-24.6 0-33.9l-136-136c-9.2-9.4-24.4-9.4-33.8 0z" 
-                      className="">
-                    </path>
-                </svg>
-            </button>
-        </div>
+        <CollapseFilters 
+          handleCollapse={handleCollapse}
+          collapseFilters={collapseFilters}
+        />
         <Sort
           data={data}
           setSortType={setSortType}
@@ -52,13 +116,17 @@ function App() {
           data={data}
           faction={faction}
           hideCollab={hideCollab}
+          forwardedRef={counterRef}
         />
-      </div>
+      </ChecklistHeader>
       <Ships
         data={data}
         setData={setData}
         faction={faction}
         setFaction={setFaction}
+        factionFilter={factionFilter}
+        typeFilter={typeFilter}
+        rarityFilter={rarityFilter}
         sortType={sortType}
         setSortType={setSortType}
         textFilter={textFilter}

0f0f5cc4509dda2af25523a23849f910f253c9cf
i-k0n <19754246+i-k0n@users.noreply.github.com>
2020-12-16 13:56:36 -0600

    feat: added click handlers for filter buttons and clearing filters

--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -1,252 +1,118 @@
-import React, { useState, useRef, useEffect } from "react";
-import { FilterButton } from "./FilterButton";
-import { FiltersContainer } from "./Filters.styles";
+import React from "react";
+import FilterButton from "./FilterButton";
+import { FiltersContainer, FactionFiltersContainer, TypeFiltersContainer, RarityFiltersContainer } from "./Filters.styles";
 import SearchFilter from "./SearchFilter";
-
-const factionFilters = [
-  {
-    full: "Eagle Union",
-    short: "eagle",
-  },
-  {
-    full: "Royal Navy",
-    short: "royal",
-  },
-  {
-    full: "Sakura Empire",
-    short: "sakura",
-  },
-  {
-    full: "Ironblood",
-    short: "ironblood",
-  },
-  {
-    full: "Dragon Empery",
-    short: "dragon",
-  },
-  {
-    full: "Sardegna Empire",
-    short: "sardegna",
-  },
-  {
-    full: "Northern Parliament",
-    short: "northern",
-  },
-  {
-    full: "Vichya Dominion",
-    short: "vichya",
-  },
-  {
-    full: "Iris Libre",
-    short: "iris",
-  },
-  {
-    full: "Universal Exchange",
-    short: "universal",
-  },
-];
-
-const typeFilters = [
-  "Vanguard",
-  "Main",
-  "DD",
-  "CL",
-  "CA",
-  "BB",
-  "CV",
-  "AR",
-  "SS",
-  "BM",
-];
-
-const rarityFilters = [
-  "Common",
-  "Rare",
-  "Elite",
-  "Super Rare",
-  "Ultra Rare",
-  "Collab",
-  "Retrofit",
-];
-
-export { factionFilters, typeFilters, rarityFilters };
+import { factionFilters, typeFilters, rarityFilters } from "./filterData";
 
 const Filters = (props) => {
-  /* eslint-disable no-unused-vars */
-  const [filteredShips, setFilteredShips] = useState(0);
-  const [totalShips, setTotalShips] = useState(0);
-  const [height, setHeight] = useState("0px")
+  const factionClick = (faction) => {
+    const filterList = [...props.factionFilter]
+    const filterIndex = filterList.indexOf(faction)
+    if (filterIndex === -1) {
+      filterList.push(faction);
+    } else {
+      filterList.splice(filterIndex, 1)
+    }
+    console.log("filter list: ", filterList)
+    props.setFactionFilter(filterList)
+  }
 
-  const content = useRef(null)
-  /* eslint-enable no-unused-vars */
+  const typeClick = (type) => {
+    const filterList = [...props.typeFilter]
+    const filterIndex = filterList.indexOf(type)
+    if (filterIndex === -1) {
+      filterList.push(type);
+    } else {
+      filterList.splice(filterIndex, 1)
+    }
+    console.log("filter list: ", filterList)
+    props.setTypeFilter(filterList)
+  }
 
-  useEffect(() => {
-    setHeight(!props.collapseFilters ? "0px" : `${content.current.scrollHeight}px`)
-  }, [props.collapseFilters])
+  const rarityClick = (rarity) => {
+    const filterList = [...props.rarityFilter]
+    const filterIndex = filterList.indexOf(rarity)
+    if (filterIndex === -1) {
+      filterList.push(rarity);
+    } else {
+      filterList.splice(filterIndex, 1)
+    }
+    console.log("filter list: ", filterList)
+    props.setRarityFilter(filterList)
+  }
 
-  // const onClick = (e, filter, type) => {
-  //   // console.log(`You clicked the ${filter} button`);
-  //   // console.log("type: ", type);
-  //   // console.log(".ship[data-" + type + "*='" + filter + "']");
 
-  //   props.setCurrentFilter(filter);
-
-  //   if (!type) {
-  //     type = "filters";
-  //   }
-
-  //   // remove disabled state on Clear Filters button
-  //   const clearFiltersButton = document.getElementById("clear-filters");
-  //   const hideToggle = document.getElementById("hide-toggle").checked;
-  //   let filterElems;
-
-  //   if (filter && filter !== " ") {
-  //     // remove disbale class from clear filters button
-  //     clearFiltersButton.classList.remove("disabled");
-
-  //     if (hideToggle) {
-  //       filterElems = document.querySelectorAll(
-  //         ".ship[data-" + type + "*='" + filter + "']:not(.completed)"
-  //       );
-  //       // set filter state to chosen filter
-  //       setFilteredShips(filterElems);
-  //     } else {
-  //       filterElems = document.querySelectorAll(
-  //         ".ship[data-" + type + "*='" + filter + "']"
-  //       );
-  //       // set filter state to chosen filter
-  //       setFilteredShips(filterElems);
-  //     }
-
-  //     // get all elements, then remove show class from them
-  //     const allElems = document.querySelectorAll(".ship");
-  //     allElems.forEach((el) => {
-  //       el.classList.remove("show");
-  //     });
-  //     // put show class back onto elements that we want filtered
-  //     filterElems.forEach((el) => {
-  //       el.classList.add("show");
-  //     });
-
-  //   }
-
-  //   document.querySelectorAll(`.filters .button`).forEach((button) => {
-  //     button.classList.remove("is-checked");
-  //   });
-  //   if (e.target) {
-  //     e.target.classList.add("is-checked");
-  //   }
-  // };
-
-  // const clearFilters = () => {
-  //   const clearFiltersButton = document.getElementById("clear-filters");
-  //   const hideToggle = document.getElementById("hide-toggle").checked;
-  //   const searchText = document.getElementById("textFilter");
-  //   const allElems = document.querySelectorAll(".ship");
-  //   const unobtainedElems = document.querySelectorAll(".ship:not(.completed)");
-
-  //   // clear currentFilter state
-  //   props.setCurrentFilter("");
-
-  //   searchText.value = " ";
-
-  //   if (hideToggle) {
-  //     // show only unobtained ships
-  //     unobtainedElems.forEach((el) => {
-  //       el.classList.remove("show");
-  //       el.classList.add("show");
-  //     });
-
-  //     clearFiltersButton.classList.add("disabled");
-  //   } else {
-  //     // show all ships
-  //     allElems.forEach((el) => {
-  //       el.classList.remove("show");
-  //       el.classList.add("show");
-  //     });
-
-  //     clearFiltersButton.classList.add("disabled");
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   // onClick(props.textFilter);
-  //   console.log(props.textFilter);
-  // }, [props.textFilter]);
+  const clearFilters = () => {
+    props.setFactionFilter([])
+    props.setTypeFilter([])
+    props.setRarityFilter([])
+    props.setTextFilter("")
+  }
 
   return (
     <FiltersContainer 
       className="filters"
       collapseFilters={props.collapseFilters}
-      ref={content}
-      heightProp={height}
+      ref={props.forwardedRef}
+      containerHeight={props.filtersContainerHeight}
     >
-      {/* <div id="faction-filters" className="button-container">
+      <FactionFiltersContainer>
         {factionFilters.map((filter, i) => {
           return (
             <FilterButton
-              // onClick={(e) => {
-              //   onClick(e, filter.short, "filters");
-              // }}
+              onClick={e => factionClick(filter.full)}
               dataFilter={filter.short}
               key={filter.short}>
               <img
-                src={`./assets/img/icon-faction-${filter.short}.png`}
+                src={require("../../assets/icons/icon-faction-" + filter.short + ".png")}
                 alt={filter.full}
               />
               {filter.full}
             </FilterButton>
           );
         })}
-      </div> */}
-      {/* <div id="type-filters" className="button-container">
+      </FactionFiltersContainer>
+      <TypeFiltersContainer id="type-filters" className="button-container">
         {typeFilters.map((filter, i) => {
           return (
             <FilterButton
-              // onClick={(e) => {
-              //   onClick(e, filter.toUpperCase(), "class");
-              // }}
+              onClick={e => typeClick(filter)}
               dataFilter={filter.toLowerCase()}
               key={filter}>
               {filter === "AR" ? "Repair" : filter}
             </FilterButton>
           );
         })}
-      </div> */}
-      <div id="rarity-filters" className="button-container">
+      </TypeFiltersContainer>
+      <RarityFiltersContainer id="rarity-filters" className="button-container">
         {rarityFilters.map((filter, i) => {
           return (
             <FilterButton
-              // onClick={(e) => {
-              //   onClick(
-              //     e,
-              //     filter.replace(/\s+/g, "-").toLowerCase(),
-              //     "filters"
-              //   );
-              // }}
+              onClick={e => rarityClick(filter)}
               dataFilter={filter.replace(/\s+/g, "-").toLowerCase()}
               key={filter}>
               {filter}
             </FilterButton>
           );
         })}
-        <button
-          id="clear-filters"
-          className="button disabled"
-          data-filter=""
-          // onClick={() => {
-          //   clearFilters();
-          // }}
+        <FilterButton
+          key={"clear-filters"}
+          disabled={props.textFilter}
+          onClick={() => {
+            clearFilters();
+          }}
         >
           Clear Filters
-        </button>
-        <SearchFilter 
+        </FilterButton>
+        
+      </RarityFiltersContainer>
+      <SearchFilter 
           setTextFilter={props.setTextFilter}
           textFilter={props.textFilter}
-          totalShips={totalShips}
-          filteredShips={filteredShips}
+          returnedShips={props.returnedShips}
+          setReturnedShips={props.setReturnedShips}
+          filteredShips={props.data}
         />
-      </div>
     </FiltersContainer>
   );
 };
